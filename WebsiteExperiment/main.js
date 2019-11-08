4e843708e1f7b2301a08503b9b544ccfeeaee0a5
Raj Agarwal <41841140+Pro-Rajinator@users.noreply.github.com>
2019-11-08 13:45:49 -0800

    Create main.js

---
+++ WebsiteExperiment/main.js
@@ -0,0 +1,12 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	frameRate(30);
+}
+
+function draw() {
+	background(200);
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
+}
