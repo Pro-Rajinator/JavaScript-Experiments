a2c3d557d8d6828adc1bfea249e1285df72575b0
Kesler <45579249+OnlineEXP@users.noreply.github.com>
2019-11-08 14:52:47 -0800

    Update main.js

--- WebsiteExperiment/main.js
+++ WebsiteExperiment/main.js
@@ -1,6 +1,23 @@
+balls = [];
+
+function ball(x, y, xSpeed, ySpeed) {
+	this.x = x;
+	this.y = y;
+	this.xSpeed = xSpeed;
+	this.ySpeed = ySpeed;
+	this.move = function() {
+		this.x += this.xSpeed;
+		this.y += this.ySpeed;
+	}
+	this.display = function() {
+		circle(this.x, this.y, 50);
+	}
+}
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	frameRate(30);
+	for(i = 0; i < 5; i++) balls.push(new ball(random(width), random(height), random(-10, 10), random(-10, 10)));
 }
 
 function draw() {
